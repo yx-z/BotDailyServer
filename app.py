a305587b2cfb769edd4c755a861d6b78334c3728
David Zhang <yx-z@outlook.com>
2021-07-10 22:16:24 -0400

    Setup flask operations

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+import datetime
+import logging
+
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-PORT = 8000
-DEBUG = True
 
-TEMPLATES = []
+TEMPLATES = {}
 
 
 @app.route("/")
@@ -14,10 +15,36 @@ def home_page():
 
 @app.route("/add_email_template", methods=["POST"])
 def add_email_template():
-    t = request.form["email_template"]
-    TEMPLATES.append(t)
-    return home_page()
+    time = request.form["time"]
+    template = request.form["email_template"]
+
+    TEMPLATES[str(len(TEMPLATES))] = (time, template)
+    return redirect("/")
+
+
+@app.route("/remove_email_template", methods=["POST"])
+def remove_email_template():
+    uid = request.form["uid"]
+    modified_time = request.form["time"]
+    modified_template = request.form["email_template"]
+    if uid in TEMPLATES:
+        if len(modified_time) == 0 or len(modified_template) == 0:
+            del TEMPLATES[uid]
+        else:
+            TEMPLATES[uid] = (modified_time, modified_template)
+    return redirect("/")
 
 
 if __name__ == "__main__":
-    app.run(port=PORT, debug=DEBUG)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)-10s %(message)s",
+        datefmt="%Y/%m/%d %H:%M:%S",
+        handlers=[
+            logging.FileHandler(
+                f"log/{datetime.datetime.today().strftime('%Y%m%d')}.log"
+            ),
+            logging.StreamHandler(),
+        ]
+    )
+    app.run()
